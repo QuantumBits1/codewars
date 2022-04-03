97e152e90722cac23b0f969cbf9182e65dc46e6b
QuantumBits1 <muhdafham8@gmail.com>
2022-04-03 18:01:08 +0800

    initial commit

---
+++ src/PrimeNumber.java
@@ -0,0 +1,49 @@
+public class PrimeNumber {
+    private long lastPrimeNumber = 0L;
+
+    public String next() {
+        setLastPrimeNumber(next(getLastPrimeNumber()));
+        return String.valueOf(getLastPrimeNumber());
+    }
+
+    public static String next(long after) {
+        long counter = after;
+
+        while(!isPrime(++counter))
+            ;
+
+        return longToString(counter);
+
+    }
+
+    public static boolean isPrime(long number) {
+        if(number <= 1)
+            return false;
+
+        if(number == 2)
+            return true;
+
+        if(number % 2 == 0)
+            return false;
+
+        long maxDivisor = (long) Math.sqrt(number);
+        for (int counter = 3; counter <= maxDivisor; counter += 2) {
+            if (number % counter == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String longToString(long number) {
+        return String.valueOf(number);
+    }
+
+    public long getLastPrimeNumber() {
+        return lastPrimeNumber;
+    }
+
+    public void setLastPrimeNumber(long lastPrimeNumber) {
+        this.lastPrimeNumber = lastPrimeNumber;
+    }
+}
---
+++ src/priorityqueue/Solution.java
@@ -0,0 +1,72 @@
+package priorityqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Student {
+    private int id;
+    private String name;
+    private double CGPA;
+
+    public Student(int id, String name, double CGPA) {
+        this.id = id;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
+
+public class Priorities {
+    private List<Student> getStudents(List<String> events) {
+        return null;
+    }
+}
+
+public class Solution {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
+
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());
+        List<String> events = new ArrayList<>();
+
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+
+        List<Student> students = priorities.getStudents(events);
+
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st: students) {
+                System.out.println(st.getName());
+            }
+        }
+    }
+}
